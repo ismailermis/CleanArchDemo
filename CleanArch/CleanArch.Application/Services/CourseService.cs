cd1d82164b3f81a5c6343d18b4c74a27988a88fb
İsmail ERMİŞ <ismail.ermis@demirorenteknoloji.com>
2019-08-16 11:24:03 +0300

    Commit implamentation of service and repository

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository _courseRespository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRespository =courseRepository;
+        }
+        
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = _courseRespository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRespository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRespository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+        public CourseRespository(UniversityDBContext dBContext)
+        {
+            _ctx=dBContext;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
